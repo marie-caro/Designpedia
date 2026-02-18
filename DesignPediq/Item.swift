f59d37166954d44d58fab7a89868d520d335495b
marie <marie-caroline.beck@epitech.eu>
2026-02-18 13:40:16 +0100

    Initial Commit

---
+++ DesignPediq/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DesignPediq
+//
+//  Created by Marie on 18/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
