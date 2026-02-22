243bad9ad3b634f9577ae5e418c446b24fa93634
marie <marie-caroline.beck@epitech.eu>
2026-02-22 14:42:25 +0100

    Initial Commit

---
+++ DesignPedia/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DesignPedia
+//
+//  Created by Marie on 22/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
